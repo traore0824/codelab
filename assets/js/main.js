76a927b6b32c4a21b4084143aec7f6db9429d331
traoré0824 <aliloulayei@gmail.com>
2024-08-07 11:03:20 +0100

    change domaine name

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
- function countdown(dateEnd) {
+function countdown() {
   var timer, days, hours, minutes, seconds;
 
-  dateEnd = new Date(dateEnd);
+  // Récupérer la date du jour
+  var dateEnd = new Date();
+
+  // Ajouter 30 jours à la date du jour
+  dateEnd.setDate(dateEnd.getDate() + 30);
+
+
   dateEnd = dateEnd.getTime();
 
-  if ( isNaN(dateEnd) ) {
+ 
+  if (isNaN(dateEnd)) {
     return;
   }
 
+ 
   timer = setInterval(calculate, 1000);
 
   function calculate() {
+    
     var dateStart = new Date();
     var dateStart = new Date(dateStart.getUTCFullYear(),
                              dateStart.getUTCMonth(),
@@ -18,9 +27,12 @@
                              dateStart.getUTCHours(),
                              dateStart.getUTCMinutes(),
                              dateStart.getUTCSeconds());
+
+  
     var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
 
-    if ( timeRemaining >= 0 ) {
+    if (timeRemaining >= 0) {
+      
       days    = parseInt(timeRemaining / 86400);
       timeRemaining   = (timeRemaining % 86400);
       hours   = parseInt(timeRemaining / 3600);
@@ -34,16 +46,25 @@
       document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
       document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
     } else {
+    
+      clearInterval(timer);
       return;
     }
   }
 
-  function display(days, hours, minutes, seconds) {}
+  function display(days, hours, minutes, seconds) {
+  }
 }
 
 window.onload = function() {
-    var preloader = document.getElementsByClassName('preloader')[0];
-    setTimeout(function(){
-        preloader.style.display = 'none';
-    }, 500);
+
+  var preloader = document.getElementsByClassName('preloader')[0];
+  
+ 
+  setTimeout(function(){
+    preloader.style.display = 'none';
+  }, 500);
+
+  // Lancer le compte à rebours
+  countdown();
 };
